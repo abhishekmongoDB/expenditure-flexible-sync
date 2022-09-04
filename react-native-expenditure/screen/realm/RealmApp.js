749012e1b0d3ab6f8147827c8a3ac423311582e7
Abhishek Sharma <abhishek.sharma@mongodb.com>
2022-09-04 05:38:58 +0530

    Removed realm id

--- react-native-expenditure/screen/realm/RealmApp.js
+++ react-native-expenditure/screen/realm/RealmApp.js
@@ -2,6 +2,6 @@ import Realm from "realm";
 
 
 // Invokes the shared instance of the Realm app.
-const app = new Realm.App({id: "expenditure-react-native-eqmpb"}); // Set Realm app ID here.
+const app = new Realm.App({id: ""}); // Set Realm app ID here.
 export default app;
 
